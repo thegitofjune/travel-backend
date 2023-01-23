7c26d87f49a67cf1c6c18e136cfd5bc05d35f6b7
cryptopandamc <junetefl@gmail.com>
2023-01-23 14:14:33 +0000

    adds application.properties with h2 console and port 8088

---
+++ src/test/java/com/june/travel/TravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.june.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
