87dbad953d5915b881dcedb1b6a0088ccf3ad2ce
June <june.mcpake@gmail.com>
2023-01-24 10:58:31 +0000

    refactors location tests and renames two variables

--- src/test/java/com/june/travel/LocationTests.java
+++ src/test/java/com/june/travel/LocationTests.java
@@ -2,6 +2,7 @@ package com.june.travel;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,41 +13,46 @@ import com.june.travel.model.Attraction;
 import com.june.travel.model.Location;
 import com.june.travel.model.Review;
 import com.june.travel.service.LocationService;
-import com.june.travel.service.ReviewService;
 
 @SpringBootTest
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class LocationTests {
-	
+
 	@Autowired
 	private LocationService locationService;
-	
+
+	private Location location;
+
+	private Review review;
+
+	private Attraction attraction;
+
+	@BeforeEach
+	void setup() {
+		location = new Location();
+		review = new Review();
+		attraction = new Attraction();
+	}
+
 	@Test
 	void test_thatALocationCanBeCreated() {
-		Location location = new Location();
 		locationService.createLocation(location);
 		assertNotEquals(0, location.getLocationId());
 	}
-	
+
 	@Test
 	void test_thatALocationCanBeCreatedWithAReview() {
-		Location location = new Location();
-		Review review = new Review();
 		location.setReviews(review);
 		locationService.createLocation(location);
 		assertNotEquals(0, location.getLocationId());
 	}
-	
+
 	@Test
 	void test_thatAnLocationCanBeSaved_withAnAttractionAndAReview() {
-		Location location = new Location();
-		Review review = new Review();
-		Attraction attraction = new Attraction();
 		location.setAttractions(attraction);
 		location.setReviews(review);
 		locationService.createLocation(location);
 		assertNotEquals(0, location.getLocationId());
 	}
-	
-	
+
 }
