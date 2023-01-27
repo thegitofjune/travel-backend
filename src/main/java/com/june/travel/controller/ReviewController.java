ca7bf83790276ea85c84a1b6cedb906109aa3549
June <june.mcpake@gmail.com>
2023-01-27 12:06:17 +0000

    adds a review controller with a get for a single review for a location, tested in talend

--- src/main/java/com/june/travel/controller/ReviewController.java
+++ src/main/java/com/june/travel/controller/ReviewController.java
@@ -2,9 +2,13 @@ package com.june.travel.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.june.travel.model.Location;
+import com.june.travel.model.Review;
 import com.june.travel.service.ReviewService;
 
 @RestController
@@ -14,6 +18,11 @@ public class ReviewController {
 	
 	@Autowired
 	private ReviewService reviewService;
+	
+	@GetMapping("/{locationId}/{reviewId}")
+	public Review findById(@PathVariable int reviewId) {
+		return reviewService.retrieveById(reviewId).get();
+	}
 	
 	
 
