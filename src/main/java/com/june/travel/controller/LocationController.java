54b8f5e58639e387e25da297f4d9f38c6ca1a5d5
June <june.mcpake@gmail.com>
2023-02-08 16:26:18 +0000

    adds a postmapping in the LocationController which has been tested and works in talend

--- src/main/java/com/june/travel/controller/LocationController.java
+++ src/main/java/com/june/travel/controller/LocationController.java
@@ -1,13 +1,18 @@
 package com.june.travel.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.june.travel.model.Location;
 import com.june.travel.service.LocationService;
@@ -19,7 +24,6 @@ public class LocationController {
 
 	@Autowired
 	private LocationService locationService;
-	
 
 	@GetMapping
 	public List<Location> allLocations() {
@@ -30,6 +34,14 @@ public class LocationController {
 	public Location findById(@PathVariable int locationId) {
 		return locationService.retrieveById(locationId).get();
 	}
-	
-	
+
+	@PostMapping("/addLocation")
+	public ResponseEntity<Location> addLocation(@RequestBody Location location) {
+		locationService.createLocation(location);
+		URI place = ServletUriComponentsBuilder.fromCurrentRequest().path("/{locationId}")
+				.buildAndExpand(location.getLocationId()).toUri();
+		return ResponseEntity.created(place).body(location);
+
+	}
+
 }
