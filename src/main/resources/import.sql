e9ac9c57e18661c14f08f5da9157c0112f2200ad
June <june.mcpake@gmail.com>
2023-03-02 20:33:52 +0000

    adds more test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,3 +6,15 @@ insert into location (locationId, name, review, rating ) values (nextval('locati
 insert into attraction (attractionId, locationId, name, rating, review ) values (nextval('attraction_seq'), 2, 'Caye Caulker', 5, 'review of caye caulker');
 insert into attraction (attractionId, locationId, name, rating, review ) values (nextval('attraction_seq'), 2, 'Belize City', 2, 'Only passed through on the way to the islands, looked a bit run down and seedy');
 
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Guatemala',  'Great for the Inka trail and some beautiful nature', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Argentina',  'Such a huge country and I only got to see a little, the people are great and you can learn to tango. Bit heavy on the meat diet though', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Peru',  'Lima is great for dance and good, then there''s Macchu Picchu of course, well worth the effort', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Chile',  'Only got to see the capital here, which is great, lots of dancing, return visit needed, ', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Easter Island',  'A Once in a lifetime trip, takes a fair bit of effort to get here but this unique place and the Rapa Nui are well worth the effort', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Uruguay',  'A small country, more stable than most in south america', 3);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'French Polynesia',  'Stunning nature, great snorkelling, eye watering prices', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Australia',  'Friendly people, lively cities and learning to scuba dive on the great barrier reef made this really special', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'New Zealand',  'Went here during the rugby world cup, lots of places fully booked but loved what I did see, nice people', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Hong Kong',  'Belize has some amazing sport for snorkelling and diving, the seafood is amazing too', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Thailand',  'Belize has some amazing sport for snorkelling and diving, the seafood is amazing too', 5);
+insert into location (locationId, name, review, rating ) values (nextval('location_seq'), 'Cambodia',  'Belize has some amazing sport for snorkelling and diving, the seafood is amazing too', 5);
